cb11abb9d1aa402570670ab43fe623d943989942
noahScottCentre <noah.scott@Centre.edu>
2023-11-03 23:22:46 +0000

    fix4

--- sql_scripts/Transaction_Insertion.sql
+++ sql_scripts/Transaction_Insertion.sql
@@ -5,6 +5,10 @@ BEGIN
 
 SELECT currentPrice INTO @PRICE FROM catalog WHERE catalogId = catalogIdIn LIMIT 1;
 
+SELECT priceOffPercent INTO @DISCOUNT FROM discounts WHERE catalogId = catalogIdIn LIMIT 1;
+
+IF (@DICOUNT != NULL) THEN @PRICE = @PRICE * @DISCOUNT;
+
 SELECT numberInStock INTO @AMOUNT FROM catalog WHERE catalogId = catalogIdIn LIMIT 1;
 
 INSERT INTO BaseTransactions (

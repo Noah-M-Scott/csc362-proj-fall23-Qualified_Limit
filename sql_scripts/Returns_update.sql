befca4584c9ba070afc81353832607790fc8fd64
upfrog <87282580+upfrog@users.noreply.github.com>
2023-11-03 23:05:52 -0400

    Create Returns_update.sql

---
+++ sql_scripts/Returns_update.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+
+CREATE PROCEDURE returnsUpdate(transactionIdIn INT, dateReturnedIn DATE)
+BEGIN
+
+UPDATE BaseReturns
+   SET transactionId = transactionIdIn, 
+       dateReturned = dateReturnedIn, 
+WHERE transactionId = transactionIdIn;
+
+END;
+//
+
+DELIMITER ;

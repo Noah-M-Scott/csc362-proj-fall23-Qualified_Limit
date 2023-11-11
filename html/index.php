f0bd02ca52b0fa0a1275217cfc3fac78447cecf4
Stephen <srr@routhouse.org>
2023-11-11 01:20:42 +0000

    git is not cooperating

---
+++ html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Upwards Outfitters</title>
+    </head>
+
+    <body>
+        <h1>Welcome!</h1>
+
+    </body>
+
+</html>
+
+<?php
+echo "Welcome to our website!"
+?>
+
